3fdb1dcec54f0be87fe1f75cfe780f9cd4f7e63d
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-01-29 14:17:43 -0300

    03 OK

--- Exercises_Odin_Javascript/02_repeatString/repeatString.js
+++ Exercises_Odin_Javascript/02_repeatString/repeatString.js
@@ -1,8 +1,15 @@
-const repeatString = function() {
-    const rep = "hey"
-    let result = rep.repeat(3)
-    return result
-};
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
